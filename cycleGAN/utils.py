a4d7703ab39b1cc0b22c86eb0fe79720bfb5bf83
ale-ubuntu <alessandro.arezzo@stud.unifi.it>
2021-02-27 18:09:21 +0100

    Bug fixed

--- cycleGAN/utils.py
+++ cycleGAN/utils.py
@@ -58,17 +58,18 @@ class Logger():
 
         # End of epoch
         if (self.batch % self.batches_epoch) == 0:
+            for loss_name, loss in self.losses.items():
+                # Reset losses for next epoch
+                self.losses[loss_name] = 0.0
             """
             # Plot losses
-            for loss_name, loss in self.losses.items():
                 if loss_name not in self.loss_windows:
                     self.loss_windows[loss_name] = self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]),
                                                                     opts={'xlabel': 'epochs', 'ylabel': loss_name, 'title': loss_name})
                 else:
                     self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]), win=self.loss_windows[loss_name], update='append')
             """
-            # Reset losses for next epoch
-            self.losses[loss_name] = 0.0
+
             self.epoch += 1
             self.batch = 1
             sys.stdout.write('\n')
